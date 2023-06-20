c17e190657fd47fd390ceca40c82bc5a20304129
Hakim Barraud <hakim.barraud@gmail.com>
2023-06-20 17:32:35 +0200

    Created dashboard page

--- src/host/Dashboard.jsx
+++ src/host/Dashboard.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import dashboard from "../images/dashboard.png";
 
 const Dashboard = () => {
-  return <div className="p-4 text-2xl font-bold">Dashboard goes here</div>;
+  return (
+    <div className="p-4 text-2xl font-bold">
+      <img src={dashboard} />
+    </div>
+  );
 };
 
 export default Dashboard;
